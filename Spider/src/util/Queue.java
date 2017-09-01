acd9392af6446e33fecdd9fa01d156fe90bd2680
Chen Xu <cxu264@usc.edu>
2017-09-02 07:31:18 +0800

    the code of spider

---
+++ Spider/src/util/Queue.java
@@ -0,0 +1,21 @@
+package util;
+import java.util.LinkedList;
+
+public class Queue {
+    private LinkedList queue=new LinkedList();
+    
+    public void enQueue(Object t){
+    	queue.addLast(t);
+    }
+    public Object deQueue(){
+    	return queue.poll();
+    }
+    public boolean isQueueEmpty(){
+    	return queue.isEmpty();
+    }
+    public boolean contains(Object t){
+    	return queue.contains(t);
+    }
+    
+    
+}
